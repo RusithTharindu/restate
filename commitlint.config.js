7630ce2d016935deab5355cbc83c64869d6770bf
RusithTharindu <rusiththarindu2@gmail.com>
2024-12-26 21:59:04 +0530

    feat: Husky init

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,13 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      ['feat', 'fix', 'chore', 'docs', 'style', 'refactor', 'test', 'revert'],
+    ],
+    'type-empty': [2, 'never'],
+    'subject-empty': [2, 'never'],
+    'subject-case': [0],
+  },
 };
